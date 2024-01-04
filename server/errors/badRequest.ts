f7d47c73210018ded2183549be366487d4f941ea
moobarrac <wahabajibola01@gmail.com>
2024-01-04 10:41:43 +0100

    created error response with status code

---
+++ server/errors/badRequest.ts
@@ -0,0 +1,12 @@
+import StatusCodes from "http-status-codes";
+import CustomAPIError from "./customError";
+
+class BadRequestError extends CustomAPIError {
+  statusCode: number;
+  constructor(message: string) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+export default BadRequestError;
---
+++ server/errors/index.ts
@@ -0,0 +1,13 @@
+import NotFoundError from "./notFound";
+import UnauthenticatedError from "./unauthenticated";
+import BadRequestError from "./badRequest";
+import CustomAPIError from "./customError";
+import UnauthorizedError from "./unauthorized";
+
+export {
+  NotFoundError,
+  UnauthenticatedError,
+  BadRequestError,
+  CustomAPIError,
+  UnauthorizedError,
+};
